1ec19995f0454937bdb4b0ea1dac4962451fecc6
Tian Zhao <61369691+TianZhao-007@users.noreply.github.com>
2021-05-17 18:16:14 +0800

    Update sequential_loop_spmv.c

--- sequential_loop_spmv.c
+++ sequential_loop_spmv.c
@@ -1,11 +1,11 @@
 for (int row =0; row < num_rows; row++)
 {
   float dot = 0;
-  int row_start = row_ptr[row];
-  int row_end = row_ptr[row+1];
+  int row_start = Ptr[row];
+  int row_end = Ptr[row+1];
   for(int elem = row_start; elem < row_end; elem++)
   {
-    dot+= data[elem] * x[col_index[elem]];
+    dot+= data[elem] * Data[Indices[elem]];
   }
   y[row]+=dot;
 }
