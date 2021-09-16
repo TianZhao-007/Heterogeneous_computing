7898e0f05dd488700391a6e799ef444019f18b3e
Tian Zhao <61369691+TianZhao-007@users.noreply.github.com>
2021-09-16 15:43:31 +0800

    Update sequential_loop_spmv.c

--- sequential_loop_spmv.c
+++ sequential_loop_spmv.c
@@ -1,3 +1,28 @@
+// num_rows – number of rows in matrix
+// ptr – the array that stores the offset to the i-th row in ptr[i]
+// indices – the array that stores the column indices for non-zero
+// values in the matrix
+// x - the dense vector
+// y - the output
+void spmv_csr_cpu(const int num_rows,
+        const int * ptr;
+        const int * indices;
+        const float * data,
+        const float * vec, float * out) {
+  
+for( int row = 0; row < num_rows, row++) {
+  float temp = 0;
+  int start_row = ptr[row];
+  int end_row = ptr[row+1];
+  for(int jj = start_row; jj < end_row; jj++)
+  temp += data[jj] * vec [indices[jj]];
+  out[row] += temp;
+}
+}
+
+
+
+/****************************************************/
 for (int row =0; row < num_rows; row++)
 {
   float dot = 0;
